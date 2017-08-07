56a5ff51b3cbb3f3a33800e9dd64d919720d4722
Max Nikolenko <maxim.nikolenko@gmail.com>
2017-08-07 12:05:49 +0300

    Refactor to a stateless functional component

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,9 @@
 const React = require('react');
 
-const Examples = React.createClass({
-  render: function () {
+const Examples = (props) => {
     return (
       <h3>Examples component</h3>
-    );
-  }
-});
+    )
+};
 
 module.exports = Examples;

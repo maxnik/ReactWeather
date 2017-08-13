95403da13d1c5f691e17b63688ebedb192d973ad
Max Nikolenko <maxim.nikolenko@gmail.com>
2017-08-13 12:03:30 +0300

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (req.protocol === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
